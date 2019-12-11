2b90449de64464a5dc005329268693ad94d8b788
Fabiano Teodoro <faateodoro@gmail.com>
2019-12-11 20:05:52 -0300

    Migração inicial

---
+++ SistemaFotos.Web/Migrations/20191211230040_Inicial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaFotos.Web.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albuns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false),
+                    Descricao = table.Column<string>(nullable: false),
+                    Caminho = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albuns", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albuns");
+        }
+    }
+}
