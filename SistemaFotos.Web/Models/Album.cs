eb67428fd76516ba1dcb21566c0457e8b2ed51cb
Fabiano Teodoro <faateodoro@gmail.com>
2019-12-11 14:53:00 -0300

    Modelo de Album

---
+++ SistemaFotos.Web/Models/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaFotos.Web.Models
+{
+    public class Album
+    {
+        public Album(string titulo, DateTime data, string descricao, string caminho)
+        {
+            Titulo = titulo;
+            Data = data;
+            Descricao = descricao;
+            Caminho = caminho;
+        }
+
+        [Required]
+        public int Id { get; private set; }
+        [Required]
+        public string Titulo { get; private set; }
+        [Required]
+        public DateTime Data { get; private set; }
+        [Required]
+        public string Descricao { get; private set; }
+        [Required]
+        public string Caminho { get; private set; }
+    }
+}
